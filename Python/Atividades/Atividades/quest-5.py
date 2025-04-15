adcd4d4b1a64de3153d490ed507df4a871e0939a
Mattheus <79262630+matthe-ai@users.noreply.github.com>
2025-04-15 17:37:43 -0300

    Delete Python/Atividades/Atividades directory

--- Python/Atividades/Atividades/quest-5.py
+++
@@ -1,9 +0,0 @@
-def temperatura():
-    temp = float(input("Quantos graus está ai: "))
-    if temp > 25:
-        print("Que calor!")
-    elif temp < 18:
-        print("Que frio!")
-    else:
-        print("Agradável")
-temperatura()
